22bfea8228afa212d989293ab20cc45eaf50cf72
Mahnoor Arif <mahnooraarif48@gmail.com>
2023-02-22 16:26:07 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+//LESS THAN (<)
+console.log("5 less than 8",5<8);
+//LESS THAN EQUALS TO (<=)
+console.log("7 less than equals 8",7<=8);
+console.log("5 less than equals 5",5<=5);
+//Relational Operators
+//Equals to (==)
+console.log("5 is equals to 5",5==5);
+console.log("8.9 is equals to 8.9",8.9==8.9);
+console.log("two strings are equal?","noor"=="noor");
+//IDENTITY EQUALS TO (===)
+console.log("9 strictly equals to 9",9===9);
+console.log("4.5 is stirctly equals to 4.5",4.5===4.5);
+console.log("two strings are equal?","noor"==="noor");
+//not equals to (!=)
+console.log("3 is not equals to 2",3!=2);
+console.log("5.5 is not equals to 5.5",5.5!=5.5);
+console.log("two string are not equal","noor"!="noor");
+//IDENTITY NOT EQUALS TO (!==)
+console.log("5 is not equals to 5",5!==5);
+console.log("3.2 is not equals to 3.2",3.2!==3.2);
+console.log("two strings are not equal","noor"!=="noor");
+//GREATER THAN (>=)
+console.log("8 is greater than equals to 6", 8>=6);
+//GREATER THAN (>)
+console.log("5 is greater than 4 ",5>4);
+
+let a = 10;
+let b = 14;
+let c = 15;
+let d = 10;
+//IF ELSE STATEMENT//
+
+if(!(a === d) && ( b === c)){
+    console.log("Condition 1 is true");
+} else if(a === b){
+    console.log("Condition 2 is truse");
+} else if( a === d){
+    console.log(" Condition 3 is truue");
+} else{
+    console.log("Nothing is true");
+}
