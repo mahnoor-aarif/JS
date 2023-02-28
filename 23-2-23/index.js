86f07e36676cfc236b1a88477ae595600ac3c700
Mahnoor Arif <mahnooraarif48@gmail.com>
2023-02-28 13:33:28 +0500

    new

---
+++ 23-2-23/index.js
@@ -0,0 +1,23 @@
+let a = 20;
+let b = 15;
+let c = 20;
+let d = 25;
+
+var sum = a + b;
+console.log("the sum of a and b is:",sum);
+ console.log("8 is equals to 8?",8==8);
+ console.log("8 is not equal to 8",8!=8);
+ console.log("5 is greater than 3",5>3);
+ console.log("true && true");
+ if(b<a && b>c){
+    console.log("yes this is true")
+ } else if(a==c || d<a){
+    console.log("MAHNOOR");
+ } else{
+    MISHAL
+ }
+ //IDENTITY EQUALS TO (===)
+ console.log("5 is identity equals to 5",5===5);
+ //IDENTITY NOT EQUALS TO (!==)
+ console.log("8 is identity not equals to 9",8!==9);
+ console.log("8 is identity equals to m",8==='m');
---
+++ 27-2-23/index.js
@@ -0,0 +1,74 @@
+//FUNCTIONS//
+function sum(a,b) {
+    console.log("the sum of these numbers are",a + b);  
+}
+console.log("MAHNOOR");
+sum(12,6);
+//FUNCTIONS
+
+function showPangram() {
+    console.log("quick brown fox jumps over the lazy dog");
+    
+}
+//caling a function//
+
+console.log("HELLO");
+showPangram();
+console.log("WELLCOME");
+ 
+//FUNCTION//
+ 
+function digitsum(num) {
+    if(num + 20 < 50){
+        console.log("MAHNOOR");
+    } else{
+        console.log("HAMNA");
+    }
+    
+}
+//CALING A FUNCTION//
+digitsum(50);
+
+
+
+//FUNCTIONS//
+function AZAN(num) {
+    if(30 - num >= 10){
+        console.log("ZAYYAN");
+    } else{
+        console.log("zayyan");
+    }
+    
+}
+
+//CALING A FUNCTION//
+
+AZAN(20);
+
+//FUNCTION//
+
+function anydash(num){
+if(num > 50 && num < 50){
+    console.log("MASHAALLAH");
+} else{
+    console.log("INSHALLAH");
+}
+}
+//CALING A FUNCTION//
+anydash(60);
+
+//FUNCTION//
+
+function mixture(a,b) {
+if(5 * a > 25 || a + 20 > 30){
+    console.log("MAHNOOR");
+} else if(7 * b >= 28 && b + 24 <=28){
+    console.log("NO NAME");
+} else{
+    console.assert("EMPTY");
+}   
+}
+
+//CALING A FUNCTION//
+
+mixture(6,4);
